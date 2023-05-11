680fd225d4b9a5c8873376ad208178e039a05789
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-05-11 21:32:17 +0200

    2 files modified

--- game/multiP/UserList.js
+++ game/multiP/UserList.js
@@ -1,23 +1,28 @@
 const socket = new WebSocket('ws://localhost:3000');
+const userOnlineDiv = document.getElementById('UserOnline');
 
-socket.addEventListener('open', function (event) {
-  socket.send(JSON.stringify({
-    type: 'login',
-    username: localStorage.getItem('username')
-  }));
-});
+// Dopo aver effettuato il login dell'utente
+socket.send(JSON.stringify({
+  type: 'login',
+  username: localStorage.getItem('username')
+}));
 
-socket.addEventListener('message', function (event) {
-  const data = JSON.parse(event.data);
+// Quando l'utente accede alla pagina degli utenti online
+socket.send(JSON.stringify({
+  type: 'getConnectedUsers'
+}));
+
+socket.on('message', (message) => {
+  const data = JSON.parse(message);
   switch (data.type) {
     case 'connectedUsers':
       const connectedUsers = data.users;
-      // Mostra gli username degli utenti connessi nella pagina
+      connectedUsers.forEach((username) => {
+        const userDiv = document.createElement('div');
+        userDiv.innerHTML = '<div style="background-color: dodgerblue; margin-inline: 5px; display: flex; flex-direction: row; justify-content: space-between; align-items: stretch; margin-top: 5px;"><x style="color: white;" id="BUser">' + username + '</x><button style="background: limegreen; border-style: revert; cursor: pointer;">Invita</button></div>';
+        userOnlineDiv.appendChild(userDiv);
+      });
       break;
     // Altri tipi di messaggi
   }
-});
-
-socket.addEventListener('error', function (event) {
-  console.log('WebSocket error', event);
 });
