311f5de46eeb97382e1a4700461c83cb27939165
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-05-31 11:10:58 +0200

    Update bot1.js

--- game/GameMood/BattleRoyal/event/bot1.js
+++ game/GameMood/BattleRoyal/event/bot1.js
@@ -3,6 +3,14 @@ let bot1 = document.getElementById("bot1");
 //Sceglie la Bocchetta e la sua evoluzione in base al rango della partita
 
 //Rileva la sua vita
-let vitaBot1 = document.getElementById("vita_bot1");
+let vitaBot1 = document.getElementById("vita_bot1").value;
+if(vitaBot1 > 0) {
+  bot1.hidden = false;
+
+  //inizia a cercare power-up
+  //...
+} else {
+  bot1.hidden = true;
+}
 
 //Inizia a camminare...
