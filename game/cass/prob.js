b8f8ce3db250b219bd5f872b4655ca0a77640d81
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-06-14 12:33:35 +0200

    2 files modified

--- game/cass/prob.js
+++ game/cass/prob.js
@@ -3,41 +3,41 @@
   const evoluz = 'https://docs.google.com/drawings/d/e/2PACX-1vQeZBD2vuiFTXswWU_AsOlwm_YpzIlCcdAessBH_-u9LhhHMX97lq2PptsKu2DUGAHUqq4wewPk8a1P/pub?w=258&h=184';
   const slotC = document.getElementById('slotC');
 
-  // 1° Caricamento
+  let intervalId;
+
+  // 1° caricamento
     let chosenVariable;
     let randomNumber = Math.random();
     if (randomNumber < 0.6) {
-      chosenVariable = tecnic;
+        chosenVariable = tecnic;
     } else {
       chosenVariable = evoluz;
     }
-    slotC.src = chosenVariable
+    slotC.src = chosenVariable;
   // END
 
-  //Listern SPACE
-  function handleKeyPress(event) {
-    if (event.code === 'Space') {
-      ABC();
-    }
-  }
-  document.addEventListener('keydown', handleKeyPress);
-  
-
-  for (let i = 0; i < 5; i++;) { 
-    setTimeout(function() {
+  function startLoop() {
+    intervalId = setInterval(function() {
       let chosenVariable;
       let randomNumber = Math.random();
-
       if (randomNumber < 0.6) {
         chosenVariable = tecnic;
       } else {
         chosenVariable = evoluz;
       }
 
-      slotC.src = chosenVariable
-      //console.log('Variabile scelta:', chosenVariable);
-    }, 1000);
+      slotC.src = chosenVariable;
+    }, 10);
   }
+
+  function handleKeyPress(event) {
+    if (event.code === 'Space') {
+      clearInterval(intervalId);
+      ABC();
+    }
+  }
+  document.addEventListener('keydown', handleKeyPress);
+  startLoop();
 
 // END
 
