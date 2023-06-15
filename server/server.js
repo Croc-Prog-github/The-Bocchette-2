7563f5c10bbf4b732bf5d7b74f250aa8ffc7ed5e
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-06-15 21:32:27 +0200

    1 file deleted

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000; // Puoi modificare la porta se necessario
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log(`Server avviato sulla porta ${port}`);
-});
