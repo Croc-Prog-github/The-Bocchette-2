db060e2cd1e1e6b03d8d4ad3610d8d2754477001
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-06-15 17:28:22 +0200

    Add files via upload

---
+++ Vanille 3/main.js
@@ -0,0 +1,5 @@
+//import './style.css' ELIMINED
+//import javascriptLogo from './javascript.svg' ELIM
+//import viteLogo from '/vite.svg'
+//import { setupCounter } from './counter.js' ELIMINED
+setupCounter(document.querySelector('#counter'))
