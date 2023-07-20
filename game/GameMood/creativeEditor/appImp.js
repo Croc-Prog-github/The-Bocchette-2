d050039e7cebe96c572bd729d5c59a88373392d0
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-20 22:01:03 +0200

    2 files modified

--- game/GameMood/creativeEditor/appImp.js
+++ game/GameMood/creativeEditor/appImp.js
@@ -1,3 +1,18 @@
 function AppImp() { //Applica impostazioni
+  //seleziona tutti gli input di: Partita
+  const QndStPICh = document.getElementById('QndStPICh').value; //Quand. Start Partit. Invia Chan.
+  const GiocMax = document.getElementById('GiocMax'); //Giocat. Max
+  const Sqad = document.getElementById('Sqad'); //Squadre
+  const DimSq = document.getElementById('DimSq'); //Dimens. Squad.
+  const  LimGen = document.getElementById('LimGen'); //Limite Gener.
+  const  DURPASq = document.getElementById('DURPASq'); //Dopo Ultim. Gener. Pas. A Squad.
+  const NumR = document.getElementById('NumR'); //Numer. Round
+  const LimTemp = document.getElementById('LimTemp'); //Limit. temp.
+  const ElimFin = document.getElementById('ElimFin'); //Eliminaz. per Finire
+  const  CSopVin = document.getElementById('CSopVin'); //Chi Soprav. Vince
+  const  SqGiocIngDP = document.getElementById('SqGiocIngDP'); //Squad. Giocatat. Ingres. Durant. partit.
+  const  LuogGen = document.getElementById('LuogGen'); //Luog. Generaz.
+  const  SpetASq = document.getElementById('SpetASq'); //Spet. Altre Squad.
 
+  console.log(QndStPICh.value);
 }
