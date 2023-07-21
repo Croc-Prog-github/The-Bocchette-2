cad2e664a9a3599590814d1cd8c4075bbc084e2f
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-07-21 09:50:05 +0200

    2 files modified

--- game/GameMood/creativeEditor/posizOgg.js
+++ game/GameMood/creativeEditor/posizOgg.js
@@ -1,3 +1,15 @@
 function posizOgg(x, y, url) {
+  var nuovoDiv = document.createElement('div');
+    
+  // Aggiungi del contenuto al nuovo div (puoi modificare il contenuto come preferisci)
+  nuovoDiv.innerHTML = 'Questo è un nuovo div creato con JavaScript!';
 
+  // Aggiungi una classe al nuovo div (opzionale, puoi rimuovere questa riga se non serve)
+  nuovoDiv.classList.add('nuovo-div');
+
+  // Ottieni il div padre con id "terr"
+  var terrDiv = document.getElementById('terr');
+
+  // Inserisci il nuovo div all'interno del div padre
+  terrDiv.appendChild(nuovoDiv);
 }
