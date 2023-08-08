cabf5d8f0521500ae344c238067b742a7bb56d32
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-08 12:41:58 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //import "/cons/cons.js";
+
+import "package.json";
+import "package-lock.json";
+
 /*
 const IDE = document.getElementsByClassName('MenuBar-MenuBar-p_D0w MenuBar-MenuBar_embedded-QBKoD');
 IDE.style.display = 'none';
