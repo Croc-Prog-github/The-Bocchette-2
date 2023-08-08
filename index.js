8433a3e3828ee3f1999d0e73de8ca59844a71fb7
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-08 12:43:40 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //import "/cons/cons.js";
 
-import "package.json";
-import "package-lock.json";
+import "express.js";
+import "json";
+import "next.js";
+import "php";
 
 /*
 const IDE = document.getElementsByClassName('MenuBar-MenuBar-p_D0w MenuBar-MenuBar_embedded-QBKoD');
