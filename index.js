d38e849dadad42b9580f06195f702633d94e7453
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-08 12:00:56 +0200

    1 file modified, 1 added

---
+++ index.js
@@ -0,0 +1,7 @@
+//import /cons/cons.js
+/*
+const IDE = document.getElementsByClassName('MenuBar-MenuBar-p_D0w MenuBar-MenuBar_embedded-QBKoD');
+IDE.style.display = 'none';
+*/
+
+window.location.href = '/load.html';
