cc5078e1ead85cd24b5e28e0606680e0a61a1f62
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-23 09:36:02 +0200

    1 file modified, 3 added, 3 deleted

---
+++ server_service/AccServ.ts
@@ -0,0 +1,30 @@
+//Script gestione account server
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
+function Salving() {
+  const fileUrl = 'https://example.com'; // URL del file da scaricare
+  const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
+
+  const downloadFile = (url: string, destination: string) => {
+    const file = fs.createWriteStream(destination);
+
+    https.get(url, (response) => {
+      response.pipe(file);
+
+      file.on('finish', () => {
+        file.close();
+        console.log('File scaricato con successo.');
+      });
+
+    })
+  
+    .on('error', (err) => {
+      fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
+      console.error('Errore durante il download:', err.message);
+    });
+  };
+
+  downloadFile(fileUrl, savePath);
+}
