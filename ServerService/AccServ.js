689560c6cf5221be64241b914fe39a09b34f7c2d
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-24 16:19:48 +0200

    2 files modified

--- ServerService/AccServ.js
+++ ServerService/AccServ.js
@@ -1,26 +1,30 @@
 // Script gestione account server
 
-import fs from 'fs';
-import https from 'https';
+function Salve() {
 
-const fileUrl = 'https://example.com'; // URL del file da scaricare
-const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
+  const fs = require('fs');
+  const https = require('https');
 
-const downloadFile = (url, destination) => {
+  const fileUrl = 'https://example.com'; // URL del file da scaricare
+  const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
+
+  const downloadFile = (url, destination) => {
   const file = fs.createWriteStream(destination);
 
-  https.get(url, (response) => {
-    response.pipe(file);
+    https.get(url, (response) => {
+      response.pipe(file);
 
-    file.on('finish', () => {
-      file.close();
-      console.log('File scaricato con successo.');
+      file.on('finish', () => {
+        file.close();
+        console.log('File scaricato con successo.');
+      });
+    })
+    .on('error', (err) => {
+      fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
+      console.error('Errore durante il download:', err.message);
     });
-  })
-  .on('error', (err) => {
-    fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
-    console.error('Errore durante il download:', err.message);
-  });
-};
+  };
 
-downloadFile(fileUrl, savePath);
+  downloadFile(fileUrl, savePath);
+
+}
--- ServerService/AccServ.ts
+++ ServerService/AccServ.ts
@@ -1,55 +1,25 @@
-//Script gestione account server
+// Script gestione account server
 import * as fs from 'fs';
 import * as path from 'path';
 import * as https from 'https';
 
-const fileUrl = 'https://example.com'; // URL del file da scaricare
-const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
+const fileUrl = 'https://example.com';  // URL del file da scaricare
+const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm';  // Percorso in cui salvare il file
 
 const downloadFile = (url: string, destination: string) => {
-  const file = fs.createWriteStream(destination);
+    const file = fs.createWriteStream(destination);
 
-  https.get(url, (response) => {
-    response.pipe(file);
-    file.on('finish', () => {
-      file.close();
-      console.log('File scaricato con successo.');
+    https.get(url, response => {
+        response.pipe(file);
+
+        file.on('finish', () => {
+            file.close();
+            console.log('File scaricato con successo.');
+        });
+    }).on('error', err => {
+        fs.unlink(destination, () => {});  // Elimina il file se si verifica un errore durante il download
+        console.error('Errore durante il download:', err.message);
     });
-
-  })
-  
-  .on('error', (err) => {
-    fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
-    console.error('Errore durante il download:', err.message);
-  });
-};
-downloadFile(fileUrl, savePath);
-
-
-/*
-// Script gestione account server
-import fs from 'fs';
-import https from 'https';
-
-const fileUrl = 'https://example.com'; // URL del file da scaricare
-const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
-
-const downloadFile = (url, destination) => {
-  const file = fs.createWriteStream(destination);
-
-  https.get(url, (response) => {
-    response.pipe(file);
-
-    file.on('finish', () => {
-      file.close();
-      console.log('File scaricato con successo.');
-    });
-  })
-  .on('error', (err) => {
-    fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
-    console.error('Errore durante il download:', err.message);
-  });
 };
 
 downloadFile(fileUrl, savePath);
-*/
