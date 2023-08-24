7eaf98ad1677b214353782a35d66dc8a8d59ffae
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-24 15:29:33 +0200

    2 files modified, 4 added, 3 deleted

---
+++ ServerService/AccServ.js
@@ -0,0 +1,25 @@
+// 
+import fs from 'fs';
+import https from 'https';
+
+const fileUrl = 'https://example.com'; // URL del file da scaricare
+const savePath = 'C:\\Users\\princ\\Desktop\\TH2_Data\\example.htm'; // Percorso in cui salvare il file
+
+const downloadFile = (url, destination) => {
+  const file = fs.createWriteStream(destination);
+
+  https.get(url, (response) => {
+    response.pipe(file);
+
+    file.on('finish', () => {
+      file.close();
+      console.log('File scaricato con successo.');
+    });
+  })
+  .on('error', (err) => {
+    fs.unlink(destination, () => {}); // Elimina il file se si verifica un errore durante il download
+    console.error('Errore durante il download:', err.message);
+  });
+};
+
+downloadFile(fileUrl, savePath);
