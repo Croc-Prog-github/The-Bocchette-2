2dcbb5e924d14cbe25520b3b641df5df56bfe714
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-08-28 23:19:54 +0200

    2 files modified

--- manut/manut.js
+++ manut/manut.js
@@ -24,4 +24,31 @@ if (window.location.pathname.endsWith('/manut/manut.html')) {
     }
     window.location.href = '/manut/manut.html';
   }, 5000);
+} else if (manut == 1) { //Disconnette forzatamente in qualunque pag html con dentro: <script src="/manut/manut.js"></script>
+  window.location.href = '/manut/manut.html';
+
+
+  const TR = document.getElementById('TR');
+  const text = 'Non molto'
+
+  if (manut == 0) {
+    TR.innerHTML = 'FATTO!';
+    setInterval(() => {
+      window.location.href = '/';
+    }, 4000)
+  } else if (manut == 1) {
+    TR.innerHTML = text;
+  }
+
+  setInterval(() => {
+    if (manut == 0) {
+      TR.innerHTML = 'FATTO!';
+      setInterval(() => {
+        window.location.href = '/';
+      }, 4000)
+    } else if (manut == 1) {
+      TR.innerHTML = text;
+    }
+    window.location.href = '/manut/manut.html';
+  }, 5000);
 }
