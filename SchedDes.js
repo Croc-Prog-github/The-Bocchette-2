ffb264ba321a2c9a1f33f0fc3fca6ce086f02268
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2023-09-20 16:02:05 +0200

    1 file modified

--- SchedDes.js
+++ SchedDes.js
@@ -1,12 +1,17 @@
-// Immagine Favicon
-const nuovoURLFavicon = 'https://docs.google.com/drawings/d/e/2PACX-1vRzg3r27e7_yLyB4m8rlyFHgQ8H5d6IJtW39axawbD5u7S_eX3ezqZdtll3NBLxJYY_OUhuSGJ-jQ8T/pub?w=16&h=16';
-const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'icon';
-link.href = nuovoURLFavicon;
-document.getElementsByTagName('head')[0].appendChild(link);
+function SchedDes() {
+  // Immagine Favicon
+  const nuovoURLFavicon = 'https://docs.google.com/drawings/d/e/2PACX-1vRzg3r27e7_yLyB4m8rlyFHgQ8H5d6IJtW39axawbD5u7S_eX3ezqZdtll3NBLxJYY_OUhuSGJ-jQ8T/pub?w=16&h=16';
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'icon';
+  link.href = nuovoURLFavicon;
+  document.getElementsByTagName('head')[0].appendChild(link);
 
-// Title della pagina
-const titleElement = document.querySelector('title');
-const titoloPagina = titleElement.textContent;
-document.title = titoloPagina;
+  // Title della pagina
+  const titleElement = document.querySelector('title');
+  const titoloPagina = titleElement.textContent;
+  document.title = titoloPagina;
+}
+setInterval(function() {
+  SchedDes();
+}, 100)
