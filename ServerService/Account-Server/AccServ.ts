aa54e75c5d8e38b907dd340555cdc422211e84e7
Croc-Prog-github <principa.gabriele@gmail.com>
2024-01-28 19:59:38 +0100

    2 file edited

--- ServerService/Account-Server/AccServ.ts
+++ ServerService/Account-Server/AccServ.ts
@@ -1,13 +1,11 @@
-//import { myFunction } from './myModule.ts';
 import * as AWS from 'aws-sdk';
 import * as fs from 'fs';
 
-
-export function myFunction() {
+export function uploadToS3() {
   // Credenziali AWS
   AWS.config.update({
     accessKeyId: 'ACCESS_KEY',
-    secretAccessKey: 'SEACRET_KEY',
+    secretAccessKey: 'SECRET_KEY',
     region: 'us-east-1',
   });
 
@@ -16,13 +14,11 @@ export function myFunction() {
   // Nome del bucket S3
   const bucketName = 'IL_TUO_BUCKET_NAME';
 
-
   // Contenuto del file TXT da archiviare
   const fileContent = 'Questo è il contenuto del file TXT archiviato in cloud.';
 
   // Nome del file TXT
   const fileName = 'il_tuo_file.txt';
-
 
   // Parametri per il caricamento del file in S3
   const params = {
@@ -30,6 +26,7 @@ export function myFunction() {
     Key: fileName,
     Body: fileContent,
   };
+
   // Carica il file in S3
   s3.upload(params, (err, data) => {
     if (err) {
@@ -39,4 +36,3 @@ export function myFunction() {
     }
   });
 }
-
