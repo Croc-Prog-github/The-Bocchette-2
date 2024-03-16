258184e315a363e33167c69bc5420cc8d05e904f
Croc-Prog-github <principa.gabriele@gmail.com>
2024-03-16 16:17:05 +0100

    19 Packages updated:
    /node_modules/.vite
    /node_modules/@colyseus
    /node_modules/@gamestdio
    /node_modules/@types
    /node_modules/body-parser
    /node_modules/call-bind
    /node_modules/colyseus
    /node_modules/esbuild
    /node_modules/express
    /node_modules/get-intrinsic
    /node_modules/has-proto
    /node_modules/ms
    /node_modules/nanoid
    /node_modules/node
    /node_modules/object-inspect
    /node_modules/postcss
    /node_modules/raw-body
    /node_modules/side-channel
    /node_modules/typescript

---
+++ node_modules/@gamestdio/timer/lib/TimerClearedError.d.ts
@@ -0,0 +1,6 @@
+/**
+ * An error that occurs when the promise of a {@link Clock.duration} is rejected because the timer has been cleared by the clock instance.
+ */
+export declare class TimerClearedError extends Error {
+    constructor();
+}
