5e5ccc932870d5f0c060fe2ac123d4df7becf31c
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-04-29 20:55:21 +0000

    3 file edited, 1 file deleted

--- ServerService/start.sh
+++ ServerService/start.sh
@@ -3,11 +3,9 @@
 # Avvia yarn install e yarn run dev in background
 (yarn install && yarn run dev) &
 
-# Attendi 5 secondi
-sleep 5
+# Attendi 1 secondi
+sleep 1
 
-# Cambia directory e avvia npm start in un nuovo terminale
+# Cambia directory e avvia npm start nello stesso terminale
 cd ServerService/MultiP-server
-
-# Avvia npm start utilizzando il terminale di macOS (Terminal.app)
-open -a Terminal.app npm start
+npm start
