183f1bcad1c46f8b39dd6387455a4731e76f5fa5
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-12 20:19:20 +0000

    1 file deleted, 2 file edited

--- game/multiP/ConnectWithCode.js
+++ game/multiP/ConnectWithCode.js
@@ -1,4 +1,5 @@
-import Colyseus from "/workspaces/The-Bocchette-2/game/multiP/colyseus.js";
+//import Colyseus from "/workspaces/The-Bocchette-2/game/multiP/colyseus.js";
+import Colyseus from "https://unpkg.com/colyseus.js@^0.15.0/dist/colyseus.js";
 
 let client = new Colyseus.Client("ws://localhost:2567");
 
