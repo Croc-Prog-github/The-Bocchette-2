aed91a2f6c265a7b43f65d033af89b001a41a455
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-12 20:05:32 +0000

    1 file edited

--- game/multiP/ConnectWithCode.js
+++ game/multiP/ConnectWithCode.js
@@ -1,3 +1,9 @@
 import Colyseus from "/workspaces/The-Bocchette-2/game/multiP/colyseus.js";
 
 let client = new Colyseus.Client("ws://localhost:2567");
+
+client.Create("room_name").then(room => {
+  console.log(room.sessionId, "joined", room.name);
+}).catch(e => {
+  console.log("JOIN ERROR", e);
+});
