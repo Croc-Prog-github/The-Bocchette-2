f56e380bf1ba2c22e67a4f5a4e8311ecf58928c2
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-12 20:39:12 +0000

    1 file edited

--- game/multiP/ConnectWithCode.js
+++ game/multiP/ConnectWithCode.js
@@ -8,3 +8,17 @@ client.Create("room_name").then(room => {
 }).catch(e => {
   console.log("JOIN ERROR", e);
 });
+
+
+room.onStateChange((state) => {
+  console.log(room.name, "has new state:", state);
+});
+room.onMessage("message_type", (message) => {
+  console.log(room.sessionId, "received on", room.name, message);
+});
+room.onError((code, message) => {
+  console.log(room.sessionId, "couldn't join", room.name);
+});
+room.onLeave((code) => {
+  console.log(room.sessionId, "left", room.name);
+});
