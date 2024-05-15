31beff847e5b0f6a80a5c98431ef6bf6f9bf7a80
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-15 17:09:45 +0000

    1 file edited

--- game/multiP/CreaSq/CreaSq_client.js
+++ game/multiP/CreaSq/CreaSq_client.js
@@ -2,13 +2,13 @@
 function CreaCodSq() {
   // Fai una richiesta al server per creare una nuova stanza
   fetch('http://127.0.0.1:2567/')
-    .then(response => response.json())
-    .then(data => {
-      // Aggiorna l'interfaccia utente con RoomId e SessionId ottenuti dal server
-      document.getElementById('roomId').textContent = data.roomId;
-      document.getElementById('sessionId').textContent = data.sessionId;
-    })
-    .catch(error => {
-      console.error('Error creating room:', error);
-    });
+  .then(response => response.json())
+  .then(data => {
+    // Aggiorna l'interfaccia utente con RoomId e SessionId ottenuti dal server
+    document.getElementById('roomId').textContent = data.roomId;
+    document.getElementById('sessionId').textContent = data.sessionId;
+  })
+  .catch(error => {
+    console.error('Error creating room:', error);
+  });
 }
