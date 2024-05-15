cc1da0efe7832679dc2427107e98eb09c539b425
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-15 17:26:56 +0000

    2 file edited

--- game/multiP/CreaSq/CreaSq_client.js
+++ game/multiP/CreaSq/CreaSq_client.js
@@ -1,5 +1,7 @@
 // Funzione per gestire la creazione di una nuova stanza
 function CreaCodSq() {
+  EntraInSq(0);
+
   // Fai una richiesta al server per creare una nuova stanza
   fetch('http://127.0.0.1:2567/')
   .then(response => response.json())
