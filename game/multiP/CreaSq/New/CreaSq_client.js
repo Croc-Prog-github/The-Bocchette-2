c120920ccd84949ca59178c6baf4ed9cf6104820
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-17 18:51:28 +0000

    2 file edited

--- game/multiP/CreaSq/New/CreaSq_client.js
+++ game/multiP/CreaSq/New/CreaSq_client.js
@@ -5,17 +5,20 @@
 const Colyseus = require('colyseus.js');
 const client = new Colyseus.Client('ws://localhost:2567');
 
-document.getElementById('createRoom').addEventListener('click', async () => {
+document.getElementById('createRoom').addEventListener('click', async () => { 
+});
+
+async function EntraInSq() {
   try {
     const room = await client.create('my_room');
     document.getElementById('roomId').innerText = room.id;
     document.getElementById('sessionId').innerText = room.sessionId;
 
     room.onJoin(() => {
-      console.log("Joined successfully", room);
+      console.log("Joined successfully: ", room);
     });
 
   } catch (e) {
     console.error("Join error", e);
   }
-});
+}
