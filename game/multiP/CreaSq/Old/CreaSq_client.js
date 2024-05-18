c49bef2d398ce6f921a207a9e7c6c138ab90fd2b
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-18 07:14:19 +0000

    2 file edited

--- game/multiP/CreaSq/Old/CreaSq_client.js
+++ game/multiP/CreaSq/Old/CreaSq_client.js
@@ -7,7 +7,7 @@ function CreaCodSq() {
   EntraInSqButt(0);
   document.getElementById('roomId').style.display = 'block';
   document.getElementById('SquadInp').value = '';
-  ToggleButton()
+  ToggleButton();
 
   // Fai una richiesta al server per creare una nuova stanza
   fetch('http://127.0.0.1:2567/')
