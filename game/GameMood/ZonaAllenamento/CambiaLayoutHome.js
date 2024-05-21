a09ec7883a2a57083a1a897216b7c27b5ae636b1
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-05-21 20:03:09 +0000

    2 file edited

--- game/GameMood/ZonaAllenamento/CambiaLayoutHome.js
+++ game/GameMood/ZonaAllenamento/CambiaLayoutHome.js
@@ -1,6 +1,6 @@
 function ZonaAddestramentoLayout() {
   if (localStorage.getItem('GameMode') == 'ZonaAddestramento') {
-    document.getElementById('M_princ').style.top = '203px';
+    document.getElementById('M_princ').style.top = '-505px';
 
     document.getElementById('Component+Slots').style.display = 'none';
     document.getElementById('BocchettaRender+Inviti').style.display = 'none';
