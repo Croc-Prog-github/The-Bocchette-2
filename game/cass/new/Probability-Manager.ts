70bec2754503b4d4a7ecce04a6db75a5afd1daf4
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-07-12 12:11:56 +0200

    1 file added

---
+++ game/cass/new/Probability-Manager.ts
@@ -0,0 +1,17 @@
+/******
+Further details on the license and usage guide are here: https://github.com/Croc-Prog-github/Probability-Manager.js
+******/
+declare module "https://raw.githubusercontent.com/Croc-Prog-github/Probability-Manager.js/main/core/Probability-Manager.js" {
+  type ProbabilityType = number | 'auto_DirectProp' | 'auto_InversProp';
+  
+  class ProbabilityManager {
+    constructor();
+    addList(instanceName: string, listName: string): void;
+    addObject(instanceName: string, listName: string, object: string, probability: ProbabilityType): void;
+    getRandomObject(instanceName: string, listName: string): string | number;
+    clearInstance(instanceName: string): void;
+    clearAll(): void;
+  }
+  
+  export default ProbabilityManager;
+}
