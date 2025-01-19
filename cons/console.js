08995d0af8071e9be75067d745faadf911f36e82
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2025-01-19 15:48:35 +0100

    1 file added, 1 file edited

---
+++ cons/console.js
@@ -0,0 +1,38 @@
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+const inp = document.getElementById('inp');
+const out = document.getElementById('out');
+
+inp.placeholder = 'digita: @, / per iniziare...';
+
+function Enter() {
+  let comand = inp.value;
+
+  if (comand == '--help') {
+    out.innerText += comand;
+    out.innerHTML += '<br>';
+    window.open('https://github.com/Croc-Prog-github/The-Bocchette-2/wiki/Guida-Terminale-The-Bocchette-2', '_blank');
+  } else if (comand == '--clear') {
+    out.innerHTML = 'The Bocchette 2 Console<br>Digita: --help per consultare la guida<br>';
+  } else if (/^@ [^\s]+$/.test(comand)) {
+    inp.placeholder = 'Digita: break per dissociare utente; comandi indirizzati a:' + comand;
+
+    out.innerText += comand;
+    out.innerHTML += '<br>';
+    out.innerText += 'Comandi indirizzati a utente:' + comand;
+    out.innerHTML += '<br>';
+  } if (comand == 'break') {
+    out.innerText += comand;
+    out.innerHTML += '<br>';
+    inp.placeholder = 'digita: @, / per iniziare...';
+  } else {
+    out.innerText += comand;
+    out.innerHTML += '<br>';
+    out.innerText += 'Comando sconosciuto.';
+    out.innerHTML += '<br>';
+  }
+
+  inp.value = '';
+}
